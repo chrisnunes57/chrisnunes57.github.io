b87e8f3cc08b55b80180f4eaf4bb0769211ef933
Chris Nunes <chrisnunes57@gmail.com>
2021-01-14 22:03:53 -0600

    deferred js files

---
+++ assets/js/Metrics.js
@@ -0,0 +1,45 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBFyWor5GDJrCBVrJ50SA79o4xfB85npE0",
+    authDomain: "nunes-metrics.firebaseapp.com",
+    projectId: "nunes-metrics",
+    storageBucket: "nunes-metrics.appspot.com",
+    messagingSenderId: "690993114565",
+    appId: "1:690993114565:web:e98a6e890947d95111afac"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+setupTracking();
+
+function setupTracking() {
+
+    let url = window.location.href;
+
+    // don't want to track localhost stats
+    // if (url.includes("localhost:"))
+    //     return;
+
+    console.log("made it ")
+
+    // update this url with page view
+    incrementPageViews(url);
+}
+
+async function incrementPageViews(url) {
+    
+    if (url.startsWith("https")) {
+        url = url.substring(8);
+    } else {
+        url = url.substring(7);
+    }
+
+    url = url.replace("/", "\\\\")
+
+    db.collection('urls').doc(url).set({ 
+        views: firebase.firestore.FieldValue.increment(1), 
+        url: url 
+    }, { merge: true });
+}
